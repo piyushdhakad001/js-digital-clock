3cbef5d035fa698d6330053aa56e2c76c281cc76
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-10 14:36:16 +0530

    Update clock every second with setInterval

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const renderTime = document.querySelector(".time");
 
-const now = new Date();
-const HH = String(now.getHours());
-const MM = String(now.getMinutes());
-const SS = String(now.getSeconds());
-renderTime.innerHTML = `${HH}:${MM}:${SS}`;
+const getTime = setInterval(() => {
+  const now = new Date();
+  const HH = String(now.getHours());
+  const MM = String(now.getMinutes());
+  const SS = String(now.getSeconds());
+  renderTime.innerHTML = `${HH}:${MM}:${SS}`
+
+},1000)
 
